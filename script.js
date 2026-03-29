31b93a9459df231aa188413cb14dcdc956033ed3
majkomel <maj.komel7@gmail.com>
2026-03-29 13:45:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ var ctx;
 var paddlex;
 var paddleh;
 var paddlew;
+var rightDown = false;
+var leftDown = false;
+var bricks;
+var NROWS;
+var NCOLS;
+var BRICKWIDTH;
+var BRICKHEIGHT;
+var PADDING;
 function init() {
   ctx = $('#canvas')[0].getContext("2d");
   WIDTH = $("#canvas").width();
@@ -42,31 +50,80 @@ function clear() {
 function draw() {
   clear();
   circle(x, y, 10);
-  x += dx;
-  y += dy;
-  clear();
+  //premik ploščice levo in desno
+  if(rightDown){
+if((paddlex+paddlew) < WIDTH){
+paddlex += 5;
+}else{
+paddlex = WIDTH-paddlew;
+}
+}
+else if(leftDown){
+if(paddlex>0){
+paddlex -=5;
+}else{
+paddlex=0;
+}
+}
+
 rect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
-circle(x, y, 10);
 
-  if (x + dx > WIDTH-r || x + dx < 0+r)
-    dx = -dx;
-
-  if (y + dy < 0+r)
-    dy = -dy;
-  else if (y + dy > HEIGHT-r) {
-    if (x > paddlex && x < paddlex + paddlew)
-      dy = -dy;
-    else
-      clearInterval(intervalId);
+//riši opeke
+  for (var i=0; i < NROWS; i++) {
+    for (var j=0; j < NCOLS; j++) {
+      if (bricks[i][j] == 1) {
+        rect((j * (BRICKWIDTH + PADDING)) + PADDING,
+            (i * (BRICKHEIGHT + PADDING)) + PADDING,
+            BRICKWIDTH, BRICKHEIGHT);
+      }
+    }
   }
 
-
-x += dx;
-
-y += dy;
+  if (x + dx > WIDTH -r || x + dx < 0+r)
+    dx = -dx;
+  if (y + dy < 0+r)
+    dy = -dy;
+  else if (y + dy > HEIGHT -(r+paddleh)) {
+    if (x > paddlex && x < paddlex + paddlew)
+      dy = -dy;
+    else if (y + dy > HEIGHT-r)
+      clearInterval(intervalId);
+  }
+  x += dx;
+  y += dy;
 }
+//nastavljanje leve in desne tipke
+function onKeyDown(evt) {
+  if (evt.keyCode == 39)
+rightDown = true;
+  else if (evt.keyCode == 37) leftDown = true;
+}
+
+function onKeyUp(evt) {
+  if (evt.keyCode == 39)
+rightDown = false;
+  else if (evt.keyCode == 37) leftDown = false;
+}
+function initbricks() { //inicializacija opek - polnjenje v tabelo
+  NROWS = 5;
+  NCOLS = 5;
+  BRICKWIDTH = (WIDTH/NCOLS) - 1;
+  BRICKHEIGHT = 15;
+  PADDING = 1;
+  bricks = new Array(NROWS);
+  for (i=0; i < NROWS; i++) {
+    bricks[i] = new Array(NCOLS);
+    for (j=0; j < NCOLS; j++) {
+      bricks[i][j] = 1;
+    }
+  }
+}
+
+$(document).keydown(onKeyDown);
+$(document).keyup(onKeyUp);  
 
 init();
 init_paddle();
+initbricks();
 
 }
